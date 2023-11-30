548c29dabb5f522ec889bcf9a929171488f0831b
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-11-30 21:08:32 +0500

    Redirect to Console homepage on disconnect

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,18 +6,27 @@ import Home from "./Home";
 import { useAppDispatch } from "@/store/store";
 import { setSelectedNavbar } from "@/store/navbarSlice";
 import Footer from "@/components/Footer";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import ContactDetails from "./ContactDetails";
 import Topbar from "@/components/dashboard/Topbar";
+import { useAccount } from "wagmi";
 
 const Operator = () => {
   const dispatch = useAppDispatch();
   const searchParams = useSearchParams()
   const contactDetails = searchParams.get('contact-details')
+  const { isDisconnected } = useAccount();
+  const router = useRouter();
 
   useEffect(() => {
     dispatch(setSelectedNavbar("dashboard"));
   }, [])
+
+  useEffect(() => {
+    if(isDisconnected) {
+      router.push("/");
+    }
+  }, [isDisconnected])
 
   return (
     <div className="w-full font-mona justify-end min-h-screen">
