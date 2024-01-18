eb3dd6311c273ba8bd41b5ae8a3b1ea1fd778175
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2024-01-18 12:30:55 +0500

    Add connect wallet onClick and Create your project link

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,11 +1,18 @@
-import Button from "@/components/ui/Button";
 import Image from "next/image";
+import Button from "@/components/ui/Button";
 import checkBalance from "@/public/check-balance.png";
 import bridge from "@/public/bridge.png";
 import stake from "@/public/stake.png";
 import exploreApps from "@/public/explore-apps.png";
+import { useAppDispatch } from "@/store/store";
+import { setIsWalletModalOpen } from "@/store/navbarSlice";
+import Link from "next/link";
+import { useAccount } from "wagmi";
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const { isConnected } = useAccount();
+
   return (
     <div className="w-full bg-light-gray flex flex-col items-center">
       <div className="w-8/9 flex flex-col mt-[130.98px] mb-[187px] md:w-9/10 max-w-7xl">
@@ -16,11 +23,18 @@ const Home = () => {
           <p className="text-xl font-normal text-text-dark-gray md:text-base mt-[22px] mb-[36.52px]">
             A one-stop-shop for everything you need
           </p>
-          <Button
-            text="Connect Wallet"
-            className="text-lg font-semibold bg-pale-green rounded-full"
-            padding="py-4 px-[52px]"
-          />
+          <div className="h-16">
+            {!isConnected &&
+              <Button
+                text="Connect Wallet"
+                className="text-lg font-semibold bg-pale-green rounded-full"
+                padding="py-4 px-[52px]"
+                onClick={() => {
+                  dispatch(setIsWalletModalOpen(true));
+                }}
+              />
+            }
+          </div>
         </div>
         <div className="bg-lightest-gray rounded-[20px] px-[83.31px] pt-[60.36px] pb-[67px] md:px-4 md:py-6 mt-[99.5px] mb-10 bg-[url('/vectors/build-mobiles.svg')] bg-no-repeat bg-bottom">
           <p className="text-[40px] leading-tight text-fuse-black font-semibold max-w-[414.86px]">
@@ -29,11 +43,12 @@ const Home = () => {
           <p className="text-xl font-normal text-text-dark-gray md:text-base max-w-[395.25px] mt-[15.42px] mb-[35.58px]">
             A one-stop-shop for everything you need
           </p>
-          <Button
-            text="Create your project"
-            className="text-lg text-white font-semibold bg-black rounded-full"
-            padding="py-4 px-[52px]"
-          />
+          <Link
+            href="/build"
+            className="text-lg text-white font-semibold bg-black rounded-full py-4 px-[52px] md:px-6"
+          >
+            Create your project
+          </Link>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-1 gap-[30.5px]">
           <div className="flex flex-row md:flex-col gap-4 justify-between bg-white rounded-[20px] px-[57px] pt-11 md:px-4 md:pt-6">
@@ -45,6 +60,7 @@ const Home = () => {
               alt="check balance"
               width={277}
               height={292}
+              className="md:m-auto"
             />
           </div>
           <div className="flex flex-row md:flex-col gap-4 justify-between bg-white rounded-[20px] px-[57px] pt-11 md:px-4 md:pt-6">
@@ -56,6 +72,7 @@ const Home = () => {
               alt="bridge"
               width={277}
               height={252}
+              className="md:m-auto"
             />
           </div>
           <div className="flex flex-row md:flex-col gap-4 justify-between bg-white rounded-[20px] px-[57px] pt-11 md:px-4 md:pt-6">
@@ -67,6 +84,7 @@ const Home = () => {
               alt="stake"
               width={277}
               height={252}
+              className="md:m-auto"
             />
           </div>
           <div className="flex flex-row md:flex-col gap-4 justify-between bg-white rounded-[20px] px-[57px] pt-11 md:px-4 md:pt-6">
@@ -78,6 +96,7 @@ const Home = () => {
               alt="explore apps"
               width={277}
               height={252}
+              className="md:m-auto"
             />
           </div>
         </div>
