7ba3ef00955163af11a010370004e333923b93cd
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-03-28 14:02:14 +0000

    Migrate from Static Export to NodeJS Server deployment

--- app/stake/[id]/page.tsx
+++ app/stake/[id]/page.tsx
@@ -1,32 +1,30 @@
-// see https://stackoverflow.com/a/77090142/12656707
+"use client";
 
-import PageWrapper from "./wrapper";
-import { getJailedValidators, getValidators } from '@/lib/contractInteract';
+import { useEffect } from "react";
+import Topbar from "@/components/Topbar";
+import Home from "./Home";
+import ChainModal from "@/components/ChainModal";
+import Footer from "@/components/Footer";
 
-export async function generateStaticParams() {
-  try {
-    // Fetch validators and jailedValidators in parallel
-    const [validators, jailedValidators] = await Promise.all([
-      getValidators(),
-      getJailedValidators(),
-    ]);
+import { useAppDispatch } from "@/store/store";
+import { setSelectedNavbar } from "@/store/navbarSlice";
 
-    // Combine and deduplicate the validator lists using a Set
-    const uniqueValidators = new Set([...validators, ...jailedValidators]);
+const Stake = () => {
+  const dispatch = useAppDispatch();
 
-    // Map the unique validators to the desired format
-    return Array.from(uniqueValidators).map(validator => ({
-      id: validator.toLowerCase(),
-    }));
-  } catch (error) {
-    console.error('Error generating static params:', error);
-    return []; // Return an empty array or handle the error as needed
-  }
-}
+  useEffect(() => {
+    dispatch(setSelectedNavbar("staking"));
+  }, [])
 
-const Stake = ({ params }: { params: { id: string } }) => {
   return (
-    <PageWrapper params={params} />
+    <div className="w-full font-mona justify-end">
+      <div className="flex-col flex items-center bg-light-gray">
+        <ChainModal />
+        <Topbar />
+        <Home />
+        <Footer />
+      </div>
+    </div>
   );
 };
 
