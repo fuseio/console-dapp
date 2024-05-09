c2b2edd26fba9a46749acc64e986339943b74c13
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-05-09 09:37:52 +0000

    Fix products min height for smaller mobile screen

--- app/Home.tsx
+++ app/Home.tsx
@@ -56,7 +56,7 @@ const Home = () => {
         <div className="grid grid-cols-3 lg:grid-cols-1 gap-[31px]">
           <Link
             href={path.STAKING}
-            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] md:min-h-[400px] bg-[url('/vectors/stake.svg')] hover:bg-[url('/vectors/stake-green.svg')] bg-no-repeat bg-bottom"
+            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] bg-[url('/vectors/stake.svg')] hover:bg-[url('/vectors/stake-green.svg')] bg-no-repeat bg-bottom"
             onClick={() => amplitude.track("Go to Staking")}
           >
             <p className="text-2xl leading-tight text-fuse-black font-bold max-w-[204.2px]">
@@ -68,7 +68,7 @@ const Home = () => {
           </Link>
           <Link
             href={path.BRIDGE}
-            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] md:min-h-[400px] bg-[url('/vectors/bridge.svg')] hover:bg-[url('/vectors/bridge-green.svg')] bg-no-repeat bg-bottom"
+            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] bg-[url('/vectors/bridge.svg')] hover:bg-[url('/vectors/bridge-green.svg')] bg-no-repeat bg-bottom"
             onClick={() => amplitude.track("Go to Bridge")}
           >
             <p className="text-2xl leading-tight text-fuse-black font-bold max-w-[204.2px]">
@@ -80,7 +80,7 @@ const Home = () => {
           </Link>
           <Link
             href={path.WALLET}
-            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] md:min-h-[400px] bg-[url('/vectors/check-balance.svg')] hover:bg-[url('/vectors/check-balance-green.svg')] bg-no-repeat bg-bottom"
+            className="transition-all ease-in-out flex flex-col gap-[17.43px] bg-lightest-gray hover:bg-success rounded-[20px] pt-[42.36px] pr-[15px] pl-[46.8px] md:px-4 md:pt-6 min-h-[420px] bg-[url('/vectors/check-balance.svg')] hover:bg-[url('/vectors/check-balance-green.svg')] bg-no-repeat bg-bottom"
           >
             <p className="text-2xl leading-tight text-fuse-black font-bold max-w-[204.2px]">
               Wallet
