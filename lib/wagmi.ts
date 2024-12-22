4bd155665d3bbed343836a1a5f2c6bce44e939cd
Lior Agnin <lior.agnin@gmail.com>
2024-12-22 15:19:08 +0200

    oops

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -12,25 +12,21 @@ const chains: readonly [Chain, ...Chain[]] = [
   base,
 ]
 
-export const evmNetworks = chains.map(chain => {
-  console.log('Icon url: ', 'https://app.dynamic.xyz/assets/networks/' + chain.name + '.svg');
-
-  return {
-    blockExplorerUrls: [chain.blockExplorers?.default?.apiUrl],
-    chainId: chain.id,
-    chainName: chain.name,
-    iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
-    name: chain.name,
-    nativeCurrency: {
-      decimals: chain.nativeCurrency.decimals,
-      name: chain.nativeCurrency.name,
-      symbol: chain.nativeCurrency.symbol,
-    },
-    networkId: chain.id,
-    rpcUrls: [...chain.rpcUrls.default.http],
-    vanityName: chain.name,
-  }
-}).map(network => ({
+export const evmNetworks = chains.map(chain => ({
+  blockExplorerUrls: [chain.blockExplorers?.default?.apiUrl],
+  chainId: chain.id,
+  chainName: chain.name,
+  iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+  name: chain.name,
+  nativeCurrency: {
+    decimals: chain.nativeCurrency.decimals,
+    name: chain.nativeCurrency.name,
+    symbol: chain.nativeCurrency.symbol,
+  },
+  networkId: chain.id,
+  rpcUrls: [...chain.rpcUrls.default.http],
+  vanityName: chain.name,
+})).map(network => ({
   ...network,
   blockExplorerUrls: network.blockExplorerUrls.filter((url): url is string => !!url)
 }));
