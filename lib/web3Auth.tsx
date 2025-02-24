e73c1452d0c2351e9ea8efe128577ee147581d2c
Mayank Mittal <msquare.pvt21@gmail.com>
2025-02-24 19:50:19 +0530

    [FIX]: RPC too Busy

--- lib/web3Auth.tsx
+++ lib/web3Auth.tsx
@@ -74,12 +74,12 @@ export const config = createConfig({
   ],
   transports: {
     [fuse.id]: http(),
-    [polygon.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
-    [mainnet.id]: http(),
+    [polygon.id]: http(`https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`),
+    [optimism.id]: http(`https://opt-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_OPTIMISM_API_KEY}`),
+    [arbitrum.id]: http(`https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ARBITRUM_API_KEY}`),
+    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ETHEREUM_API_KEY}`),
     [bsc.id]: http(),
-    [base.id]: http(),
+    [base.id]: http(`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_BASE_API_KEY}`),
     [flash.id]: http(),
   },
 })
