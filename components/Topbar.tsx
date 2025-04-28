a40a6a8325bf1672c407fb7219734654d243c7cd
Nikolay <dnolnque@gmail.com>
2025-04-28 14:40:10 +0300

    fixes

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,76 +1,70 @@
-import { useCallback, useMemo, useState } from "react";
+import {useMemo, useState} from "react";
 import fuseConsoleLogo from "@/assets/fuse-console-logo.svg";
 import fuseLogoMobile from "@/assets/logo-mobile.svg";
 import NavMenu from "./NavMenu";
 import NavButton from "./NavButton";
-import { useAppDispatch, useAppSelector } from "@/store/store";
-import { selectNavbarSlice, setIsWalletModalOpen } from "@/store/navbarSlice";
-import { cn, path } from "@/lib/helpers";
+import {useAppSelector} from "@/store/store";
+import {selectNavbarSlice} from "@/store/navbarSlice";
+import {cn, path} from "@/lib/helpers";
 import Image from "next/image";
-import { useAccount } from "wagmi";
-import { useRouter } from "next/navigation";
 
 type TopbarProps = {
   className?: string;
-}
+};
 
-const Topbar = ({ className }: TopbarProps) => {
+const Topbar = ({className}: TopbarProps) => {
   const [isOpen, setOpen] = useState<boolean>(false);
-  const { isTransfiModalOpen, selected } = useAppSelector(selectNavbarSlice);
-  const dispatch = useAppDispatch();
-  const { isConnected } = useAccount();
-  const router = useRouter();
+  const {isTransfiModalOpen, selected} = useAppSelector(selectNavbarSlice);
 
-  const handlePointsClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    if (isConnected) {
-      router.push(path.AIRDROP_ECOSYSTEM);
-    } else {
-      dispatch(setIsWalletModalOpen(true));
-    }
-  }, [dispatch, isConnected, router]);
+  const AirdropSubmenu = useMemo(
+    () => [
+      {
+        title: "Fuse Ember Testnet",
+        link: path.AIRDROP_GRANT,
+      },
+      {
+        title: "Points",
+        link: path.AIRDROP_ECOSYSTEM,
+      },
+      {
+        title: "Testnet Nodes",
+        link: path.TESTNET_NODES,
+      },
+    ],
+    []
+  );
 
-  const AirdropSubmenu = useMemo(() => [
-    {
-      title: "Fuse Ember Testnet",
-      link: path.AIRDROP_GRANT,
-    },
-    {
-      title: "Points",
-      link: "#",
-      onClick: handlePointsClick
-    },
-    {
-      title: "Testnet Nodes",
-      link: path.TESTNET_NODES,
-    },
-  ], [handlePointsClick]);
-
-  const menuItems = useMemo(() => [
-    {
-      title: "Home",
-      link: path.HOME,
-    },
-    {
-      title: "Ember",
-      link: path.AIRDROP,
-      submenu: AirdropSubmenu
-    },
-    {
-      title: "Bridge",
-      link: path.BRIDGE,
-    },
-    {
-      title: "Staking",
-      link: path.STAKING,
-    },
-  ], [AirdropSubmenu]);
+  const menuItems = useMemo(
+    () => [
+      {
+        title: "Home",
+        link: path.HOME,
+      },
+      {
+        title: "Ember",
+        link: path.AIRDROP,
+        submenu: AirdropSubmenu,
+      },
+      {
+        title: "Bridge",
+        link: path.BRIDGE,
+      },
+      {
+        title: "Staking",
+        link: path.STAKING,
+      },
+    ],
+    [AirdropSubmenu]
+  );
 
   return (
-    <nav className={cn("w-full h-20 sticky top-0 backdrop-blur-xl flex justify-center py-7 md:h-[32px] md:mt-2 border-b-[0.5px] border-pastel-gray md:border-0",
-      isTransfiModalOpen ? "z-0" : "z-40",
-      className
-    )}>
+    <nav
+      className={cn(
+        "w-full h-20 sticky top-0 backdrop-blur-xl flex justify-center py-7 md:h-[32px] md:mt-2 border-b-[0.5px] border-pastel-gray md:border-0",
+        isTransfiModalOpen ? "z-0" : "z-40",
+        className
+      )}
+    >
       <div className="flex justify-between h-full items-center w-8/9 md:w-9/10 max-w-7xl relative">
         <div className="flex items-center gap-10">
           <span>
@@ -91,7 +85,12 @@ const Topbar = ({ className }: TopbarProps) => {
               />
             </a>
           </span>
-          <NavMenu menuItems={menuItems} isOpen={isOpen} selected={selected} isResponsive />
+          <NavMenu
+            menuItems={menuItems}
+            isOpen={isOpen}
+            selected={selected}
+            isResponsive
+          />
         </div>
         <NavButton isOpen={isOpen} setOpen={setOpen} />
       </div>
